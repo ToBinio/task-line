4a7346202011951b5d1851831506f3e3d435af2b
tobinio <Tobias.frischmann1@gmail.com>
2025-04-02 07:21:32 +0200

    use nuxt 4 folder structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/icon", "@pinia/nuxt", "@vueuse/nuxt"],
