5a3b4408d50fb3dcc14f558496fc7bec5bab3e42
tobinio <Tobias.frischmann1@gmail.com>
2025-04-05 13:55:20 +0200

    use popover for adding tags

--- app/components/settings/SettingsSheet.vue
+++ app/components/settings/SettingsSheet.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import {
+    PopoverContent,
+    PopoverPortal,
+    PopoverRoot,
+    PopoverTrigger,
+    PopoverArrow,
+} from "reka-ui";
 import Sheet from "../utils/Sheet.vue";
 
 let isOpen = defineModel<boolean>("isOpen", { required: true });
@@ -29,8 +36,64 @@ function onAddTag() {
         <div class="p-1">
             <h2 class="text-lg text-stone-400">Tags</h2>
             <div class="flex pt-1 pb-2 gap-1 overflow-scroll">
+                <PopoverRoot>
+                    <PopoverTrigger
+                        class="aspect-square rounded border-1 border-stone-300 dark:border-stone-700 px-1 flex items-center border-dashed"
+                    >
+                        <Icon name="material-symbols:add-2-rounded" size="20" />
+                    </PopoverTrigger>
+                    <PopoverPortal>
+                        <PopoverContent
+                            class="flex flex-col gap-2 z-50 bg-stone-700 p-2 rounded-lg mx-2 drop-shadow-lg/30"
+                            side="top"
+                            :sideOffset="5"
+                        >
+                            <input
+                                class="pl-1 border-1 border-stone-300 dark:border-stone-600 rounded text-lg w-36"
+                                placeholder="new Tag"
+                                type="text"
+                                v-model="tagName"
+                            />
+                            <div
+                                class="h-7.5 flex border-1 border-stone-300 dark:border-stone-600 rounded"
+                            >
+                                <button
+                                    class="flex justify-center items-center aspect-square"
+                                    @click="onRandomizeColor"
+                                >
+                                    <Icon
+                                        name="material-symbols:autorenew-rounded"
+                                        class="text-stone-500 hover:text-stone-400 transition-colors"
+                                        size="20"
+                                    />
+                                </button>
+                                <input
+                                    v-model="color"
+                                    class="rounded-sm h-full w-full"
+                                    type="color"
+                                />
+                            </div>
+                            <button
+                                class="border-1 rounded border-stone-300 dark:border-stone-600 px-0.5 flex justify-center items-center bg-stone-700 hover:bg-stone-600 transition-colors"
+                                @click="onAddTag"
+                            >
+                                <Icon
+                                    name="material-symbols:add-2-rounded"
+                                    class="text-stone-300 hover:text-stone-200 transition-colors"
+                                    size="20"
+                                />
+                            </button>
+                            <PopoverArrow
+                                class="fill-stone-700"
+                                :height="10"
+                                :width="20"
+                                :rounded="true"
+                            />
+                        </PopoverContent>
+                    </PopoverPortal>
+                </PopoverRoot>
                 <div
-                    class="rounded border-1 border-stone-300 dark:border-stone-700 pl-1 flex"
+                    class="rounded border-1 border-stone-300 dark:border-stone-700 pl-1 flex gap-0.5"
                     v-for="tag in tagStore.data"
                     :style="{ color: tag.color }"
                     :key="tag.uuid"
@@ -43,43 +106,6 @@ function onAddTag() {
                         />
                     </button>
                 </div>
-            </div>
-            <div class="flex h-8 gap-2">
-                <input
-                    class="pl-1 border-1 border-stone-300 dark:border-stone-700 rounded text-lg w-36"
-                    placeholder="new Tag"
-                    type="text"
-                    v-model="tagName"
-                />
-                <div
-                    class="flex h-full border-1 border-stone-300 dark:border-stone-700 rounded"
-                >
-                    <button
-                        class="px-0.5 flex justify-center items-center"
-                        @click="onRandomizeColor"
-                    >
-                        <Icon
-                            name="material-symbols:autorenew-rounded"
-                            class="text-stone-500 hover:text-stone-400 transition-colors"
-                            size="20"
-                        />
-                    </button>
-                    <input
-                        v-model="color"
-                        class="h-7.5 rounded-sm"
-                        type="color"
-                    />
-                </div>
-                <button
-                    class="border-1 rounded aspect-square border-stone-300 dark:border-stone-700 px-0.5 flex justify-center items-center bg-stone-700 hover:bg-stone-600 transition-colors"
-                    @click="onAddTag"
-                >
-                    <Icon
-                        name="material-symbols:add-2-rounded"
-                        class="text-stone-300 hover:text-stone-200 transition-colors"
-                        size="20"
-                    />
-                </button>
             </div>
         </div>
     </Sheet>
--- app/components/utils/Sheet.vue
+++ app/components/utils/Sheet.vue
@@ -18,6 +18,7 @@ const emit = defineEmits<{ close: [] }>();
         <DrawerPortal>
             <DrawerOverlay
                 class="fixed h-dvh w-dvw top-0 left-0 backdrop-blur-[1.5px] z-20"
+                @click="emit('close')"
             />
             <DrawerContent
                 class="fixed bottom-0 rounded-t-lg w-dvw h-96 bg-stone-200 dark:bg-stone-800 z-30 p-1 pt-2 flex flex-col gap-2"
