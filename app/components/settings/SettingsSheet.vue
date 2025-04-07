07d9765eddae375f273eb2c007facfe2e755b618
tobinio <Tobias.frischmann1@gmail.com>
2025-04-07 13:46:14 +0200

    add tag delete verification

--- app/components/settings/SettingsSheet.vue
+++ app/components/settings/SettingsSheet.vue
@@ -15,23 +15,8 @@ function close() {
         <div class="p-1 pt-0">
             <h2 class="text-lg text-stone-400">Tags</h2>
             <div class="flex pt-1 pb-2 gap-1 overflow-scroll">
-                <SettingsAddTodo />
-                <div
-                    class="h-8 rounded border-1 border-stone-300 dark:border-stone-700 pl-1 flex items-center gap-0.5"
-                    v-for="tag in tagStore.data"
-                    :style="{ color: tag.color }"
-                    :key="tag.uuid"
-                >
-                    <div class="text-nowrap">
-                        {{ tag.name }}
-                    </div>
-                    <button class="h-6" @click="tagStore.deleteTag(tag.uuid)">
-                        <Icon
-                            name="material-symbols:delete-outline-rounded"
-                            size="24"
-                        />
-                    </button>
-                </div>
+                <SettingsAddTag />
+                <SettingsTag v-for="tag in tagStore.data" :tag="tag" />
             </div>
         </div>
     </Sheet>
---
+++ app/components/settings/Tag.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import {
+    PopoverTrigger,
+    PopoverRoot,
+    PopoverPortal,
+    PopoverArrow,
+    PopoverContent,
+} from "reka-ui";
+
+defineProps<{ tag: Tag }>();
+
+let tagStore = useTagStore();
+</script>
+<template>
+    <div
+        class="h-8 rounded border-1 border-stone-300 dark:border-stone-700 pl-1 flex items-center gap-0.5"
+        :style="{ color: tag.color }"
+        :key="tag.uuid"
+    >
+        <div class="text-nowrap">
+            {{ tag.name }}
+        </div>
+        <PopoverRoot>
+            <PopoverTrigger class="h-6">
+                <Icon
+                    name="material-symbols:delete-outline-rounded"
+                    size="24"
+                />
+            </PopoverTrigger>
+            <PopoverPortal>
+                <PopoverContent
+                    class="flex flex-col gap-2 z-50 bg-stone-700 p-2 rounded-lg mx-2 drop-shadow-lg/30"
+                    side="top"
+                    :sideOffset="5"
+                >
+                    <button class="h-6" @click="tagStore.deleteTag(tag.uuid)">
+                        <Icon
+                            name="material-symbols:delete-outline-rounded"
+                            size="24"
+                        />
+                    </button>
+                    <PopoverArrow
+                        class="fill-stone-700"
+                        :height="10"
+                        :width="20"
+                        :rounded="true"
+                    />
+                </PopoverContent>
+            </PopoverPortal>
+        </PopoverRoot>
+    </div>
+</template>
