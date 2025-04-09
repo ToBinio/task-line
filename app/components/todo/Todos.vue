42b717dc9cde2c8580bf1fe9dab40e628819dec5
tobinio <Tobias.frischmann1@gmail.com>
2025-04-09 16:22:55 +0200

    make desktop width useable

--- app/components/todo/Todos.vue
+++ app/components/todo/Todos.vue
@@ -18,17 +18,19 @@ const filterdTodos = computed(() => {
 </script>
 
 <template>
-  <div class="mt-3 mb-10 w-dvw flex-1 px-1 py-3">
-    <TransitionGroup
-      leave-to-class="max-h-0! opacity-0 scale-y-0"
-      enter-from-class="max-h-0! opacity-0 scale-y-0"
-    >
-      <Todo
-        v-for="todo in filterdTodos"
-        :key="todo.uuid"
-        class="max-h-16 transition-all duration-300 ease-in"
-        :data="todo"
-      />
-    </TransitionGroup>
+  <div class="flex justify-center">
+    <div class="mt-3 mb-10 w-dvw max-w-200 overflow-x-hidden px-1 py-3">
+      <TransitionGroup
+        leave-to-class="max-h-0! opacity-0 scale-y-0"
+        enter-from-class="max-h-0! opacity-0 scale-y-0"
+      >
+        <Todo
+          v-for="todo in filterdTodos"
+          :key="todo.uuid"
+          class="max-h-16 transition-all duration-300 ease-in"
+          :data="todo"
+        />
+      </TransitionGroup>
+    </div>
   </div>
 </template>
--- app/components/utils/Sheet.vue
+++ app/components/utils/Sheet.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
-import { VisuallyHidden } from "reka-ui";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogPortal,
+  DialogRoot,
+  DialogTitle,
+  VisuallyHidden,
+  DialogOverlay,
+} from "reka-ui";
 import {
   DrawerRoot,
   DrawerPortal,
@@ -12,9 +20,11 @@ import {
 
 defineProps<{ isOpen: boolean; title: string }>();
 const emit = defineEmits<{ close: [] }>();
+
+const useModal = useMediaQuery("(min-width: 40rem)");
 </script>
 <template>
-  <DrawerRoot :open="isOpen" @close="emit('close')">
+  <DrawerRoot v-if="!useModal" :open="isOpen" @close="emit('close')">
     <DrawerPortal>
       <DrawerOverlay
         class="fixed top-0 left-0 z-20 h-dvh w-dvw backdrop-blur-[1.5px]"
@@ -32,4 +42,26 @@ const emit = defineEmits<{ close: [] }>();
       </DrawerContent>
     </DrawerPortal>
   </DrawerRoot>
+  <DialogRoot v-if="useModal" :open="isOpen" @close="emit('close')">
+    <DialogPortal>
+      <DialogOverlay
+        class="fixed top-0 left-0 z-20 h-dvh w-dvw backdrop-blur-[1.5px]"
+        @click="emit('close')"
+      />
+      <Transition
+        enter-from-class="opacity-0 scale-90"
+        leave-to-class="opacity-0 scale-95"
+      >
+        <DialogContent
+          class="fixed top-1/2 left-1/2 z-30 h-96 w-9/10 max-w-128 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-stone-200 p-1 duration-200 dark:bg-stone-800"
+        >
+          <VisuallyHidden>
+            <DialogTitle>{{ title }}</DialogTitle>
+            <DialogDescription>{{ title }}</DialogDescription>
+          </VisuallyHidden>
+          <slot />
+        </DialogContent>
+      </Transition>
+    </DialogPortal>
+  </DialogRoot>
 </template>
