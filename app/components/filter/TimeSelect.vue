45d8319e0a2e28e5495bfb1d9ca39b6910d3191a
tobinio <Tobias.frischmann1@gmail.com>
2025-05-17 19:21:46 +0200

    style timeSelect.vue

--- app/components/filter/TimeSelect.vue
+++ app/components/filter/TimeSelect.vue
@@ -1,43 +1,34 @@
 <script setup lang="ts">
-import { RadioGroupRoot, RadioGroupItem, RadioGroupIndicator } from "reka-ui";
+import { RadioGroupRoot, RadioGroupItem } from "reka-ui";
 
 const time = defineModel<"all" | "today" | "week">("time", { required: true });
 </script>
 
 <template>
-  <RadioGroupRoot v-model="time" class="flex gap-2">
-    <div class="flex items-center gap-1">
+  <RadioGroupRoot v-model="time" class="flex">
+    <div>
       <RadioGroupItem
-        class="h-[1.125rem] w-[1.125rem] cursor-default rounded-full border bg-white shadow-sm outline-none focus:shadow-[0_0_0_2px] focus:shadow-stone-700 data-[active=true]:border-stone-700 data-[active=true]:bg-stone-700 dark:data-[active=true]:bg-white"
+        class="data-[active]:bg-secondary-hover border-secondary cursor-pointer rounded-l border-1 px-1"
         value="all"
       >
-        <RadioGroupIndicator
-          class="relative flex h-full w-full items-center justify-center after:block after:h-2 after:w-2 after:rounded-[50%] after:bg-white after:content-[''] dark:after:bg-stone-700"
-        />
+        All
       </RadioGroupItem>
-      <label> All </label>
     </div>
-    <div class="flex items-center gap-1">
+    <div>
       <RadioGroupItem
-        class="h-[1.125rem] w-[1.125rem] cursor-default rounded-full border bg-white shadow-sm outline-none focus:shadow-[0_0_0_2px] focus:shadow-stone-700 data-[active=true]:border-stone-700 data-[active=true]:bg-stone-700 dark:data-[active=true]:bg-white"
+        class="data-[active]:bg-secondary-hover border-secondary cursor-pointer border-y-1 px-1"
         value="today"
       >
-        <RadioGroupIndicator
-          class="relative flex h-full w-full items-center justify-center after:block after:h-2 after:w-2 after:rounded-[50%] after:bg-white after:content-[''] dark:after:bg-stone-700"
-        />
+        Today
       </RadioGroupItem>
-      <label> Today </label>
     </div>
-    <div class="flex items-center gap-1">
+    <div>
       <RadioGroupItem
-        class="h-[1.125rem] w-[1.125rem] cursor-default rounded-full border bg-white shadow-sm outline-none focus:shadow-[0_0_0_2px] focus:shadow-stone-700 data-[active=true]:border-stone-700 data-[active=true]:bg-stone-700 dark:data-[active=true]:bg-white"
+        class="data-[active]:bg-secondary-hover border-secondary cursor-pointer rounded-r border-1 px-1"
         value="week"
       >
-        <RadioGroupIndicator
-          class="relative flex h-full w-full items-center justify-center after:block after:h-2 after:w-2 after:rounded-[50%] after:bg-white after:content-[''] dark:after:bg-stone-700"
-        />
+        This Week
       </RadioGroupItem>
-      <label> This Week </label>
     </div>
   </RadioGroupRoot>
 </template>
