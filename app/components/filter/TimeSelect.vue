85ad9bd97e1105f79a0f3b598d7c83b1567403ac
tobinio <Tobias.frischmann1@gmail.com>
2025-05-18 09:47:51 +0200

    make width of timeSelection consistend

--- app/components/filter/TimeSelect.vue
+++ app/components/filter/TimeSelect.vue
@@ -5,30 +5,24 @@ const time = defineModel<"all" | "today" | "week">("time", { required: true });
 </script>
 
 <template>
-  <RadioGroupRoot v-model="time" class="flex">
-    <div>
-      <RadioGroupItem
-        class="data-[active]:bg-secondary-hover border-secondary cursor-pointer rounded-l border-1 px-1"
-        value="all"
-      >
-        All
-      </RadioGroupItem>
-    </div>
-    <div>
-      <RadioGroupItem
-        class="data-[active]:bg-secondary-hover border-secondary cursor-pointer border-y-1 px-1"
-        value="today"
-      >
-        Today
-      </RadioGroupItem>
-    </div>
-    <div>
-      <RadioGroupItem
-        class="data-[active]:bg-secondary-hover border-secondary cursor-pointer rounded-r border-1 px-1"
-        value="week"
-      >
-        This Week
-      </RadioGroupItem>
-    </div>
+  <RadioGroupRoot v-model="time" class="flex max-w-48">
+    <RadioGroupItem
+      class="data-[active]:bg-secondary-hover border-secondary flex-1 cursor-pointer rounded-l border-1 px-1"
+      value="all"
+    >
+      All
+    </RadioGroupItem>
+    <RadioGroupItem
+      class="data-[active]:bg-secondary-hover border-secondary flex-1 cursor-pointer border-y-1 px-1"
+      value="today"
+    >
+      Today
+    </RadioGroupItem>
+    <RadioGroupItem
+      class="data-[active]:bg-secondary-hover border-secondary flex-1 cursor-pointer rounded-r border-1 px-1"
+      value="week"
+    >
+      Week
+    </RadioGroupItem>
   </RadioGroupRoot>
 </template>
