0343982109dbde42f09b82e4669adf6775e2c197
tobinio <Tobias.frischmann1@gmail.com>
2025-05-18 10:02:45 +0200

    hide task without date when selecting timeFrame

--- app/components/todo/Todos.vue
+++ app/components/todo/Todos.vue
@@ -14,23 +14,24 @@ const filterdTodos = computed(() => {
       }
     }
 
-    if (todo.timeframe) {
-      const start = parseDate(todo.timeframe.start);
-      const end = parseDate(todo.timeframe.end);
+    if (filterStore.time === "all") return true;
+    if (!todo.timeframe) return false;
 
-      if (filterStore.time === "today") {
-        const now = today(getLocalTimeZone());
+    const start = parseDate(todo.timeframe.start);
+    const end = parseDate(todo.timeframe.end);
 
-        if (start.compare(now) > 0 || end.compare(now) < 0) {
-          return false;
-        }
-      } else if (filterStore.time === "week") {
-        const weekStart = today(getLocalTimeZone());
-        const weekEnd = weekStart.add({ weeks: 1 });
+    if (filterStore.time === "today") {
+      const now = today(getLocalTimeZone());
 
-        if (start.compare(weekEnd) > 0 || end.compare(weekStart) < 0) {
-          return false;
-        }
+      if (start.compare(now) > 0 || end.compare(now) < 0) {
+        return false;
+      }
+    } else if (filterStore.time === "week") {
+      const weekStart = today(getLocalTimeZone());
+      const weekEnd = weekStart.add({ weeks: 1 });
+
+      if (start.compare(weekEnd) > 0 || end.compare(weekStart) < 0) {
+        return false;
       }
     }
 
