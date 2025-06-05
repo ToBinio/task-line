634e899b271a8b8f660f3d2179a062a0ca5652be
tobinio <Tobias.frischmann1@gmail.com>
2025-06-05 10:09:49 +0200

    translate mobile filter

--- app/components/FilterSheet.vue
+++ app/components/FilterSheet.vue
@@ -6,6 +6,8 @@ import TimeSelect from "./filter/TimeSelect.vue";
 
 const isOpen = defineModel<boolean>("isOpen", { required: true });
 
+const { t } = useI18n();
+
 const { filter } = useFilter();
 
 function close() {
@@ -16,15 +18,15 @@ function close() {
 <template>
   <Sheet :is-open="isOpen" title="Filter Sheet" @close="close">
     <div class="p-1 pt-0">
-      <h2 class="text-muted-text text-lg">Categories</h2>
+      <h2 class="text-muted-text text-lg">{{ t("categories") }}</h2>
       <CategorySelect v-model:category="filter.category" />
     </div>
     <div class="p-1 pt-0">
-      <h2 class="text-muted-text text-lg">Tags</h2>
+      <h2 class="text-muted-text text-lg">{{ t("tags") }}</h2>
       <TagSelect v-model:tags="filter.tags" :show-all="false" />
     </div>
     <div class="p-1">
-      <h2 class="text-muted-text text-lg">Time</h2>
+      <h2 class="text-muted-text text-lg">{{ t("time") }}</h2>
       <TimeSelect v-model:time="filter.time" />
     </div>
   </Sheet>
